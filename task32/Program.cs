6350509a780fb03d1aad8517d01c86ab36da7d4b
Asia84 <84asia@mail.ru>
2022-07-10 20:42:24 +0700

    32 задача, замена на противоположные знаки в массиве, красивый вывод

---
+++ task32/Program.cs
@@ -0,0 +1,18 @@
+﻿/*32/Замена элементов массива:положительные элементы 
+замените на соответствующие отрицательные, и наоборот.
+[-4, -8, 8, 2] ->[4, 8, -8, -2]
+*/
+int[] array = new int[4];
+for (int i = 0; i < 4; i++)
+{ 
+  array[i] = new Random().Next(-9, 10);  
+}
+//Console.WriteLine();
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+for(int i = 0; i < 4; i++)
+{
+    array[i] = array[i] * (-1);
+}
+Console.WriteLine("[" + string.Join(", ", array) + "]");//красиво выводим массив без использования цикла
+//Console.WriteLine();
+
