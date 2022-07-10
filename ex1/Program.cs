29ee6689e8f194e69d4029b0c9bfffe36ace8210
Asia84 <84asia@mail.ru>
2022-07-10 14:25:39 +0700

    пример 1

---
+++ ex1/Program.cs
@@ -0,0 +1,27 @@
+﻿/*void factorial( int[] n)
+{
+    int count = 1;
+    for(int i = 1;i <=n;i++)
+    {
+        count = count * i;
+    }
+Console.WriteLine(count);
+}
+
+factorial(5);
+*/
+
+
+
+int factorial( int n)
+{
+    int count = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        count = count * i;
+    }
+    return count;
+}
+int result;
+result = factorial(5);
+Console.WriteLine(result);
