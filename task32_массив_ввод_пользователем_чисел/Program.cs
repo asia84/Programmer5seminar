843809f09783ee914b90cbdd48726e810a23678e
Asia84 <84asia@mail.ru>
2022-07-10 21:05:20 +0700

    32 задача с вводом данных от пользователя

---
+++ task32_массив_ввод_пользователем_чисел/Program.cs
@@ -0,0 +1,17 @@
+﻿/*32/Замена элементов массива:положительные элементы 
+замените на соответствующие отрицательные, и наоборот.
+[-4, -8, 8, 2] ->[4, 8, -8, -2]
+*/
+int[] array = new int[5];
+for (int i = 0; i < array.Length; i++)
+{ 
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+for(int i = 0; i < array.Length; i++)
+//(int i = 0; i < 4; i++)
+{
+    array[i] = array[i] * (-1);
+}
+Console.WriteLine("[" + string.Join(", ", array) + "]");//красиво выводим массив без использования цикла
+//Console.WriteLine();
