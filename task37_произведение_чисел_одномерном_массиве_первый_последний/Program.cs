50060a302036a084ba56050e532a4e86d9e1317f
Asia84 <84asia@mail.ru>
2022-07-10 23:01:33 +0700

    37 задача произведение чисел последнего и первого в массиве, заданном пользователем

--- task37_произведение_чисел_одномерном_массиве_первый_последний/Program.cs
+++ task37_произведение_чисел_одномерном_массиве_первый_последний/Program.cs
@@ -7,7 +7,37 @@ for(int i = 0; i < array.Length; i++)
 { 
     array[i] = Convert.ToInt32(Console.ReadLine());
 }
-for(int i = 0; i < array.Length / 2 + 0.5 ; i++)
+int len;
+if(n % 2 == 0) len = n / 2 - 1;
+else len = n / 2;
+for(int i = 0; i <= len  ; i++)
+
+//for(int i = 0; i <= array.Length / 2 ; i++)
 { 
     Console.WriteLine(array[i] * array[array.Length - i - 1]);
 }
+
+
+
+
+
+/*
+int n;
+Console.WriteLine("Напишите кол-во чисел: ");
+n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+Console.WriteLine("Напишите числа: ");
+for (int i = 0; i < array.Length; i++)
+{
+array[i] = Convert.ToInt32(Console.ReadLine());
+}
+int len;
+if (n % 2 == 0){
+len = n / 2 - 1;
+}
+else {len = n / 2;}
+for (int i = 0; i <= len; i++)
+{
+Console.WriteLine(array[i] * array[array.Length - i - 1]);
+}
+*/
