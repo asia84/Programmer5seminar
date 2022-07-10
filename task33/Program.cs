4bc9fc56fc515476cd23764bdf7bf8703c5d8da3
Asia84 <84asia@mail.ru>
2022-07-10 21:18:34 +0700

    33 задача(заданный массив пользователем,поиск заданного числа в этом массиве)

---
+++ task33/Program.cs
@@ -0,0 +1,31 @@
+﻿/*33/Задайте массив. Напишите программу,которая определяет,
+ присутствует ли данное число в массиве:
+4; массив [6, 7, 9, 5, -3,]-> нет
+-3; массив [6, 7, -3, 2, 1]-> да
+*/
+int[] array = new int[5];
+for (int i = 0; i < array.Length; i++)
+{ 
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("Введите число: ");
+int number;
+bool log  = false;
+number = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i < array.Length; i++)
+{ 
+    if(number == array[i])//число равно числу массива
+    {
+        log = true;//число совпадает
+        break;//выходим из цикла
+    }
+}
+if (log)
+{
+    Console.WriteLine("Вот оно наконец-то!");
+}
+else
+{
+    Console.WriteLine("Этого числа в массиве нет..");
+}
+
